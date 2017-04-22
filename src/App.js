564a892b58679d0804b014396845310693fabf29
Jake Fishbain <jakefishbain@gmail.com>
2017-04-22 15:24:38 -0500

    Create score and input

--- src/App.js
+++ src/App.js
@@ -3,18 +3,32 @@ import Scoreboard from './Scoreboard';
 import './App.css';
 var uuid = require('node-uuid');
 
+const addScore = (id, score, boards) => {
+  function attachScore(board){
+    if(board.id === id){
+      board.score = score
+    }
+    return board
+  }
+  return boards.map(attachScore)
+}
+
 class App extends Component {
   constructor(props){
     super(props);
     this.state = {
       boards: [
-        { id: 0, name: 'name' },
+        { id: 0, score: 0 },
       ]
     }
   }
 
   handleAddBoard(props){
-    this.setState({boards: this.state.boards.concat({id: uuid.v4(), name: 'name'})})
+    this.setState({boards: this.state.boards.concat({id: uuid.v4(), score: 0})})
+  }
+
+  handleAddScore(id, score){
+    this.setState({boards: addScore(id, score, this.state.boards)})
   }
 
   render() {
@@ -28,7 +42,11 @@ class App extends Component {
           <ul>
             {
               this.state.boards.map( board =>(
-                <Scoreboard key={board.id} name={board.name}/>
+                <Scoreboard
+                  key={board.id}
+                  score={board.score}
+                  onAddScore={this.handleAddScore.bind(this)}
+                />
               ))
             }
           </ul>
--- src/Scoreboard.js
+++ src/Scoreboard.js
@@ -2,10 +2,20 @@ import React, { Component } from 'react'
 // import './App.css';
 
 class Scoreboard extends Component {
+  addScore(e){
+    // console.log(e.target.value)
+    this.props.onAddScore(this.props.id, e.target.value)
+  }
+
   render(){
     return(
       <div className='container'>
-        <h4>{this.props.name}</h4>
+        <input className='player'/>
+        <h2>{this.props.score}</h2>
+        <input
+          className='addScore'
+          onChange={this.addScore.bind(this)}
+        />
       </div>
     )
   }
