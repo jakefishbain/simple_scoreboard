2a205000d9cd133b39b9e01a404b566a213f3b2e
Jake Fishbain <jakefishbain@gmail.com>
2017-04-23 17:51:17 -0500

    Move credits

--- src/App.js
+++ src/App.js
@@ -133,6 +133,7 @@ class App extends Component {
       <div className="App">
 
         <div className="AppHeader">
+          <div className='credits'>Icons made by <a href="http://www.flaticon.com/authors/vignesh-oviyan" title="Vignesh Oviyan">Vignesh Oviyan</a> from <a href="http://www.flaticon.com" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
           <h2>Simple Scoreboard</h2>
         </div>
           <button className='addBoardBtn' onClick={this.handleAddBoard.bind(this)}>Add a Board</button>
@@ -157,7 +158,6 @@ class App extends Component {
               ))
             }
             </div>
-      <div className='credits'>Icons made by <a href="http://www.flaticon.com/authors/vignesh-oviyan" title="Vignesh Oviyan">Vignesh Oviyan</a> from <a href="http://www.flaticon.com" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
       </div>
     );
   }
