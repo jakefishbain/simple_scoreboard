df3878ac4feda9b8055f4672b62a8c0c70c61e50
Jake Fishbain <jakefishbain@gmail.com>
2017-04-22 22:03:32 -0500

    Change title and icon

--- src/App.js
+++ src/App.js
@@ -78,6 +78,7 @@ class App extends Component {
               ))
             }
           </ul>
+      <div className='credits'>Icons made by <a href="http://www.flaticon.com/authors/vignesh-oviyan" title="Vignesh Oviyan">Vignesh Oviyan</a> from <a href="http://www.flaticon.com" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
       </div>
     );
   }
